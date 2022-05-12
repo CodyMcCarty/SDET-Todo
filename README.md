7eba237dba9a4d7ddd015acee7f48c8ed3ae4a6d
Cody McCarty <cmccarty@catalyte.io>
2022-05-12 13:13:03 -0500

    prep for refactor

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TODO test with Selenium
+# TodoApp test with Selenium
 For the SEDT ADT training 1.1
 
 ## Usage
@@ -6,4 +6,6 @@ Clone the repository at https://github.com/filiphric/udemy-cypress-course
 and use it's README to run it locally.  
 Clone this repository.  
 Run the test in test/TodoTest.java or run `mvn test` in the terminal.  
+
+Lint using Google's Java coding standards by pressing ctrl + alt + L
 
--- src/test/java/TodoTest.java
+++ src/test/java/TodoTest.java
@@ -34,15 +34,15 @@ public class TodoTest {
     chromeDriver.quit();
   }
 
-  /* 1) GIVEN I am at the todo page
+  /* 1) GIVEN I am at the todoPage
 WHEN I add text then press enter
 THEN It is added to the list */
 
   @Test //TODO: get code review
   public void addTodoUpdatesTodoListV1Test() {
-    chromeDriver.get("http://localhost:3000/");
+    chromeDriver.get("http://localhost:3000/"); // FIXME move to setup
     WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(10));
-    WebElement todoInput = wait.until(
+    WebElement todoInput = wait.until( // this is impciet
         ExpectedConditions.presenceOfElementLocated(By.id("add-todo"))
     );
     String todoExpected = new SimpleDateFormat("HH.mm.ss").format(new java.util.Date());
@@ -52,7 +52,8 @@ THEN It is added to the list */
     todoInput.sendKeys(Keys.RETURN);
 
     wait.until(
-        ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"todo-list\"]/li")));
+        // FIXME change from xpath to cssSelector
+        ExpectedConditions.visibilityOfAllElementsLocatedBy(By. xpath("//*[@id=\"todo-list\"]/li")));
     List<WebElement> todos = chromeDriver.findElements(By.xpath("//*[@id=\"todo-list\"]/li"));
     WebElement todoActual;
     try {
@@ -60,6 +61,7 @@ THEN It is added to the list */
           .stream()
           .filter(e -> e.getText().contains(todoExpected))
           .findFirst().get();
+      // FIXME throw exception message about not finding the todoAdded / change to .assert
     } catch (Exception e) {
       todoActual = todos.get(todos.size() - 1);
     }
@@ -73,18 +75,18 @@ THEN It is added to the list */
 
 
 
-/*2) GIVEN I am at the todo page
+/*2) GIVEN I am at the todoPage
   AND there is an item on the list
   WHEN I hover over the item
   AND X out on the item
   THEN the item is removed */
 
-/*3) GIVEN I am at the todo page
+/*3) GIVEN I am at the todoPage
   WHEN I click on the circle next to the item
   THEN the item is crossed out
   AND the item is greyed out */
 
-/*4) GIVEN I am at the todo page
+/*4) GIVEN I am at the todoPage
   WHEN I hover over an item
   AND I click on the X next to the item
   THEN the list will collapse
